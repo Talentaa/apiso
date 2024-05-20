7693247a2193b161f6404a9204dc021ce938bdad
AA <talentyouth99@gmail.com>
2024-05-20 10:12:52 +0800

    add connection DAOs to axum state

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,10 +1,15 @@
-use axum::{response::IntoResponse, Json};
+use axum::{extract::State, response::IntoResponse, Json};
 use time::OffsetDateTime;
 use uuid::Uuid;
 
-use crate::models::*;
+use crate::{models::*, AppState};
 
-pub async fn create_question(Json(question): Json<Question>) -> impl IntoResponse {
+mod handlers_inner;
+
+pub async fn create_question(
+    State(AppState { questions_dao, .. }): State<AppState>,
+    Json(question): Json<Question>,
+) -> impl IntoResponse {
     Json(QuestionDetail {
         title: question.title,
         description: question.description,
@@ -13,7 +18,9 @@ pub async fn create_question(Json(question): Json<Question>) -> impl IntoRespons
     })
 }
 
-pub async fn read_questions() -> impl IntoResponse {
+pub async fn read_questions(
+    State(AppState { questions_dao, .. }): State<AppState>,
+) -> impl IntoResponse {
     Json(vec![QuestionDetail {
         title: "title".to_string(),
         description: "description".to_string(),
@@ -22,9 +29,16 @@ pub async fn read_questions() -> impl IntoResponse {
     }])
 }
 
-pub async fn delete_question(Json(question_uuid): Json<QuestionId>) {}
+pub async fn delete_question(
+    State(AppState { questions_dao, .. }): State<AppState>,
+    Json(question_uuid): Json<QuestionId>,
+) {
+}
 
-pub async fn create_answer(Json(answer): Json<Answer>) -> impl IntoResponse {
+pub async fn create_answer(
+    State(AppState { answers_dao, .. }): State<AppState>,
+    Json(answer): Json<Answer>,
+) -> impl IntoResponse {
     Json(AnswerDetail {
         answer_uuid: Uuid::new_v4(),
         question_uuid: answer.question_uuid,
@@ -33,7 +47,9 @@ pub async fn create_answer(Json(answer): Json<Answer>) -> impl IntoResponse {
     })
 }
 
-pub async fn read_answers() -> impl IntoResponse {
+pub async fn read_answers(
+    State(AppState { answers_dao, .. }): State<AppState>,
+) -> impl IntoResponse {
     Json(vec![AnswerDetail {
         question_uuid: Uuid::new_v4(),
         content: "content".to_string(),
@@ -42,4 +58,8 @@ pub async fn read_answers() -> impl IntoResponse {
     }])
 }
 
-pub async fn delete_answer(Json(question_id): Json<QuestionId>) -> impl IntoResponse {}
+pub async fn delete_answer(
+    State(AppState { answers_dao, .. }): State<AppState>,
+    Json(question_id): Json<QuestionId>,
+) -> impl IntoResponse {
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
+use std::sync::Arc;
+
 use axum::{
     routing::{delete, get, post},
     Router,
 };
 use dotenvy::dotenv;
 use handlers::*;
+use persistance::{
+    answers_dao::{AnswersDao, AnswersDaoImpl},
+    questions_dao::{QuestionsDao, QuestionsDaoImpl},
+};
 use sqlx::postgres::PgPoolOptions;
 use tokio::net::TcpListener;
 
 mod handlers;
 mod models;
 mod persistance;
+
+#[derive(Clone)]
+pub struct AppState {
+    pub questions_dao: Arc<dyn QuestionsDao + Send + Sync>,
+    pub answers_dao: Arc<dyn AnswersDao + Send + Sync>,
+}
 
 #[tokio::main]
 async fn main() {
@@ -24,13 +36,22 @@ async fn main() {
         .await
         .expect("Connot connect to Database");
 
+    let questions_dao = Arc::new(QuestionsDaoImpl::new(pool.clone()));
+    let answers_dao = Arc::new(AnswersDaoImpl::new(pool));
+
+    let app_state = AppState {
+        questions_dao,
+        answers_dao,
+    };
+
     let app = Router::new()
         .route("/question", post(create_question))
         .route("/question", delete(delete_question))
         .route("/questions", get(read_questions))
         .route("/answer", post(create_answer))
         .route("/answer", delete(delete_answer))
-        .route("/answers", get(read_answers));
+        .route("/answers", get(read_answers))
+        .with_state(app_state);
 
     let listener = TcpListener::bind("127.0.0.1:8000").await.unwrap();
 
